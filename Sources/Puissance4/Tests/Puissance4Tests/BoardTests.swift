c84b807ed12d526e02a74fd40a30a526c6d65642
etudiant <valentin-clergue@orange.fr>
2023-01-17 16:27:00 +0100

    fix tests

--- Sources/Puissance4/Tests/Puissance4Tests/BoardTests.swift
+++ Sources/Puissance4/Tests/Puissance4Tests/BoardTests.swift
@@ -4,7 +4,7 @@ import XCTest
 final class PBoardTests: XCTestCase {
     func testExample() throws {
         let board: Board? = Board(Rows: 8, Columns: 10)
-        XCTAssertEqual(board!.nbRows, 10)
-        XCTAssertEqual(board!.nbColumns, 8)
+        XCTAssertEqual(board!.nbRows, 8)
+        XCTAssertEqual(board!.nbColumns, 10)
     }
 }
